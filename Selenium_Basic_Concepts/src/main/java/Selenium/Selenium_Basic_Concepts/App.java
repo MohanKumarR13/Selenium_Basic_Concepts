5ec84e20abda940666f49d2e79ccfdb24b5737da
MohanKumarR13 <svgvedha@gmail.com>
2023-05-21 21:34:13 +0530

    Selenium Basic Concepts

---
+++ Selenium_Basic_Concepts/src/main/java/Selenium/Selenium_Basic_Concepts/App.java
@@ -0,0 +1,13 @@
+package Selenium.Selenium_Basic_Concepts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
